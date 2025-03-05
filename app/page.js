4e520f1d044e8c81ed47e014080e2ae2bef58f34
tarkeshwar raj <tarkeshwarraj@gmail.com>
2025-03-05 21:46:29 +0530

    services components

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 import Header from "@/components/Header";
 import Navbar from "../components/Navbar";
 import About from "@/components/About";
+import Services from "@/components/Services";
 
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <Navbar/>
       <Header/>
       <About/>
+      <Services/>
    </>
   );
 }
