e95caff4296fc2f5ceb5a3bd2dcd60f940d47889
tarkeshwar raj <tarkeshwarraj@gmail.com>
2025-03-05 20:22:33 +0530

    tools

--- components/About.jsx
+++ components/About.jsx
@@ -1,4 +1,4 @@
-import { assets, infoList } from '@/assets/assets'
+import { assets, infoList, toolsData } from '@/assets/assets'
 import React from 'react'
 import Image from 'next/image'
 
@@ -16,16 +16,24 @@ const About = () => {
             <p className='mb-10 max-w-2xl font-Ovo'>I am an experienced Frontend Developer with over a decade of professional expertise in the field. Throughout my career, I have had the privilege of collaborating with prestigious organizations, contributing to their success and growth.</p>
 
             <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
+
               {infoList.map(({icon, iconDark, title, description}, index)=>(
-                
-                <li className='border-[0.5px] border-gary-400 rounded-xl p-6 cursor-pointer' key={index}>
+                <li className='border-[0.5px] border-gary-400 rounded-xl p-6 cursor-pointer hover:lightHover hover:-translate-y-1 duration-500' key={index}>
                   <Image src={icon} alt={title} className='w-7 mt-3' />
                   <h3 className='my-4 font-semibold text--gray-700'>{title}</h3>
                   <p className='text-gray-600 text-sm'>{description}</p>
-
-                </li>
-                
+                </li>   
                 ))}
+
+            </ul>
+
+            <h4 className='my-6 text-gray-700 font-Ovo'>Tools I use</h4>
+            <ul className='flex items-center gap-3 sm:gap-5'>
+              {toolsData.map((tool, index)=>(
+                <li className='flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500' key={index}>
+                  <Image src={tool} alt='Tool' className='w-5 sm:w-7'/>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
